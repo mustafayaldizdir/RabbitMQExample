bf588724f61791f074bba2a80a3bce067874922c
Mustafa Emin Yaldızdır <mustafa.yaldizdir.dn@aygaz.com.tr>
2022-04-19 13:33:03 +0300

    Producer model added.

---
+++ RabbitMQExample.Producer/Producers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQExample.Producer
+{
+    public class Producers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
